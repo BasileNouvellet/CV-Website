3c0ca00bcb936777c08107e00fdfeaa60727feab
Basile Nouvellet <basile.nouvellet@ponts.org>
2018-02-07 09:42:06 +0100

    Initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBe6PF9h0KZuFFMaJinsM0GgIcM6_of88E",
+    authDomain: "basile-nouvellet-resume.firebaseapp.com",
+    databaseURL: "https://basile-nouvellet-resume.firebaseio.com",
+    projectId: "basile-nouvellet-resume",
+    storageBucket: "",
+    messagingSenderId: "832401104980"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBe6PF9h0KZuFFMaJinsM0GgIcM6_of88E",
+    authDomain: "basile-nouvellet-resume.firebaseapp.com",
+    databaseURL: "https://basile-nouvellet-resume.firebaseio.com",
+    projectId: "basile-nouvellet-resume",
+    storageBucket: "",
+    messagingSenderId: "832401104980"
+  }
+};
