2cf58c9345ccdd2225c25307fa982d5c2c895da7
Basile Nouvellet <basile.nouvellet@ponts.org>
2018-02-27 14:44:34 +0100

    Add social media icon

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,9 +7,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
   }
 
+  copyTextToClipboard(text) {
+    var txtArea = document.createElement("textarea");
+
+    txtArea.style.position = 'fixed';
+    txtArea.style.top = '0';
+    txtArea.style.left = '0';
+    txtArea.style.opacity = '0';
+    txtArea.value = text;
+    document.body.appendChild(txtArea);
+    txtArea.select();
+    try {
+      var successful = document.execCommand('copy');
+      var msg = successful ? 'successful' : 'unsuccessful';
+      console.log('Copying text command was ' + msg);
+      if(successful){
+        return true;
+      }
+    } catch (err) {
+      console.log('Oops, unable to copy');
+    }
+    document.body.removeChild(txtArea);
+    return false;
+  }
+
+  copyMailToClipboard() {
+    var textToCopy = "basile.nouvellet@ponts.org";
+    let result = this.copyTextToClipboard(textToCopy);
+    if (result) {
+      console.log('Copied to Clipboard');
+    }
+  }
+
 }
+
+
