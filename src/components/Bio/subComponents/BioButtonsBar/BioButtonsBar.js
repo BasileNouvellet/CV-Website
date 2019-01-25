ba4c328a5c1ef3d7f184df2b9d9fc29475ef5aa2
Basile Nouvellet <basile.nouvellet@ponts.org>
2019-01-25 14:48:30 +0100

    Finish Bio BUtton Bar

--- src/components/Bio/subComponents/BioButtonsBar/BioButtonsBar.js
+++ src/components/Bio/subComponents/BioButtonsBar/BioButtonsBar.js
@@ -9,17 +9,15 @@ type ButtonPropsType = {|
   onClick: () => void;
 |};
 
+const openNewTab = (link: string) => () => { window.open(link, '_blank'); };
+
 function Button(props: ButtonPropsType) {
   const { text, onClick } = props;
 
   return (
-    <button
-      className="btn-call-to-action"
-      type="button"
-      onClick={onClick}
-    >
+    <div className="btn-call-to-action" onClick={onClick}>
       {text}
-    </button>
+    </div>
   );
 }
 
@@ -28,12 +26,12 @@ function BioButtonsBar() {
     <div id="BioButtonsBar">
       <Button
         text="Download CV"
-        onClick={() => null}
+        onClick={openNewTab('./assets/Resume_Basile_NOUVELLET.pdf')}
       />
 
       <Button
         text="Contact Me"
-        onClick={() => null}
+        onClick={openNewTab('mailto:basile.nouvellet@ponts.org')}
       />
     </div>
   );
--- src/components/Bio/subComponents/BioText/BioText.js
+++ src/components/Bio/subComponents/BioText/BioText.js
@@ -7,11 +7,11 @@ import './BioText.css';
 function BioText() {
   return (
     <div id="BioText">
-      <div className="title">
+      <div className="bio-title">
         I am looking for a 6-month internship, starting in February 2019
       </div>
 
-      <div className="text">
+      <div className="bio-text">
         My name is Basile, I am currently studying Financial engineering and Computer Science
         at Ecole des Ponts ParisTech, one of the top French engineering schools.
         I have an entrepreneurial mindset with strong problem solving skills, a UX Design
