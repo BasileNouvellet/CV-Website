6f9c388fc40b4bf25020f5f4247ca308c9dd74c3
Basile Nouvellet <basile.nouvellet@ponts.org>
2019-01-25 15:01:55 +0100

    Fix text in section item card

--- src/components/SectionTimeline/SectionTimeline.js
+++ src/components/SectionTimeline/SectionTimeline.js
@@ -19,11 +19,8 @@ type PropsType = {|
 
 // TODO: display logo only if display not shrunk
 
-function renderSectionTimeline(
-  items: Array<SectionItemType>,
-  colors: ColorsObjectType,
-) {
-  return items.map((item: SectionItemType, index: number) => {
+function renderSectionItem(colors: ColorsObjectType) {
+  return (item: SectionItemType, index: number) => {
     const position = index % 2 === 0 ? 'left' : 'right';
     const lineClass = `line ${position}`;
     const middleLineStyle = { backgroundColor: colors.second };
@@ -37,9 +34,9 @@ function renderSectionTimeline(
         />
 
         <img
-          src={item.logo}
-          alt={`${item.company.toLowerCase()} logo`}
           className="logo"
+          src={item.logo}
+          alt={`${item.company} logo`}
         />
 
         <div className="middle-line-container">
@@ -47,7 +44,7 @@ function renderSectionTimeline(
         </div>
       </div>
     );
-  });
+  };
 }
 
 function SectionTimeline(props: PropsType) {
@@ -59,7 +56,7 @@ function SectionTimeline(props: PropsType) {
     <div id="SectionTimeline" style={titleStyle}>
       <div className="section-timeline-title">{title}</div>
 
-      {renderSectionTimeline(items, colors)}
+      {items.map(renderSectionItem(colors))}
     </div>
   );
 }
